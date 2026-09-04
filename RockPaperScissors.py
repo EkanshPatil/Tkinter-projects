f2a9d9b65f0b3f20e0656d107385167d1558eac7
Ekansh Patil <ekansh.patil23@gmail.com>
2026-09-04 17:04:10 +0100

    Add files via upload

---
+++ RockPaperScissors.py
@@ -0,0 +1,37 @@
+import tkinter
+import random
+screen = tkinter.Tk()
+options = ["rock","paper","scissors"]
+def game(player_choice):
+    computer_choice = random.choice(options)
+    computer_label1.config(text=f"Computer selected: {computer_choice}")
+    player_label1.config(text=f"You selected: {player_choice}")
+screen.geometry("1000x1000")
+label1 = tkinter.Label(screen,text="Rock Paper Scissors!",font=("New Times Roman",50,"bold"),fg="gray")
+label1.grid(row=1,column=2,columnspan=7)
+winner_label = tkinter.Label(screen,fg="green")
+winner_label.grid(row=2,column=3)
+label2 = tkinter.Label(screen,fg="gray",text="Your Options:",font=(20))
+label2.grid(row=2,column=1)
+
+rock_button = tkinter.Button(screen,bg="red",text="Rock",fg="white",font=("bold"),command=game("rock"))
+rock_button.grid(row=3,column=2)
+paper_button = tkinter.Button(screen,bg="gray",text="Paper",fg="white",font=("bold"),command=game("paper"))
+paper_button.grid(row=3,column=3)
+scissors_button = tkinter.Button(screen,bg="blue",text="Scissors",fg="white",font=("bold"),command=game("scissors"))
+scissors_button.grid(row=3,column=4)
+
+label3 = tkinter.Label(screen,fg="gray",text="score:",font=(20))
+label3.grid(row=4,column=1)
+
+player_label1 = tkinter.Label(screen,text="You selected: ",font=(10))
+player_label1.grid(row=5,column=2)
+player_label2 = tkinter.Label(screen,text="Your score: ",font=(10))
+player_label2.grid(row=5,column=3)
+
+computer_label1 = tkinter.Label(screen,text="Computer selected: ",font=(10))
+computer_label1.grid(row=6,column=2)
+computer_label2 = tkinter.Label(screen,text="Computer score: ",font=(10))
+computer_label2.grid(row=6,column=3)
+
+screen.mainloop()
